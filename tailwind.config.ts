4b1c81fc0435f4fbe2c1b315c47c0a5e5f20ec3c
S-Khraban <S.Hraban@i.ua>
2025-11-19 01:08:45 +0200

    Fix header navigation and mobile menu scroll lock & add breakpoints for TW4 (#33)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,23 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
     "./index.html",
     "./src/**/*.{ts,tsx,js,jsx}",
   ],
+
   theme: {
     screens: {
       sm: "320px",
-
       md: "640px",
-
       lg: "1200px",
-
       xl: "1440px",
     },
 
-    extend: {
-    },
+    extend: {},
   },
 
   plugins: [],
 };
+
+export default config;
