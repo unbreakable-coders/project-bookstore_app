3f5aa9c8b5fe15e3e5f7ba7980fdec418d7d0d42
Maksym Shevchuk <47719190+MaksimShe@users.noreply.github.com>
2025-11-19 00:08:06 +0200

    UC34: implemented (#23)
    * added recommendation bundle skeleton
    
    * UC-34: added basic markup
    
    * UC34: implemented banner without mobile version
    
    * UC34:implemented Promo and Categories with responsibility
    
    * UC34 implemented promo and catigories with responsibility
    
    * UC34: refactored abd fixed comments
    
    ---------
    
    Co-authored-by: SheFing <shevchukm2002@gmail.com>
    Co-authored-by: S-Khraban <s.hraban@i.ua>

---
+++ src/components/organisms/Home/Categories.tsx
@@ -0,0 +1,34 @@
+import { Category, type CategoryType } from '@/components/molecules/Category';
+
+export const Categories = () => {
+  const categories: CategoryType[] = [
+    {
+      title: 'Paper books',
+      countOfBooks: '1,044 books',
+      videoSrc: 'public/books/img/categories/paper.mp4',
+    },
+    {
+      title: 'Audio books',
+      countOfBooks: '344 books',
+      videoSrc: 'public/books/img/categories/audio.mp4',
+    },
+    {
+      title: 'Kindle books',
+      countOfBooks: '265 books',
+      videoSrc: 'public/books/img/categories/kindlebook.mp4',
+    },
+  ];
+
+  return (
+    <div className="flex flex-col justify-center items-center px-4 w-full max-w-[1200px] m-auto">
+      <h2 className="w-full mb-6 text-2xl font-bold text-center">
+        Shop by category
+      </h2>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 justify-center max-lg:grid-cols-3 max-sm:grid-cols-1">
+        {categories.map(category => (
+          <Category category={category} key={category.title} />
+        ))}
+      </div>
+    </div>
+  );
+};
