e082d16fbd97af5927114fb13dce03a8deb886f9
Victoriia Kodovbetska <113036948+Pobedasg@users.noreply.github.com>
2025-11-19 23:58:14 +0200

    UC-79: Create Cart Page (#41)
    * Create Cart Page
    
    * feat: implement add to cart functionality with UAH price conversion
    
    * UC-79: refactor Cart page with useCart hook and cart components
    
    * UC-79: fix eslint errors and clean useCart
    
    ---------
    
    Co-authored-by: S-Khraban <s.hraban@i.ua>

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -9,8 +9,8 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default:
-          'bg-primary text-primary-foreground shadow hover:bg-primary/90',
+        default: 'bg-primary text-card shadow hover:bg-primary/90 border-none',
+        added: 'bg-card text-ring border border-muted hover:bg-card/90',
         destructive:
           'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
         outline:
--- src/components/atoms/Price/Price.tsx
+++ src/components/atoms/Price/Price.tsx
@@ -15,7 +15,7 @@ export const Price: React.FC<PriceProps> = ({
     old: 'text-sm text-gray-400 line-through',
   };
 
-  return <span className={styles[variant]}>₴{amount}</span>;
+  //return <span className={styles[variant]}>₴{amount}</span>;
   //return <span className={styles[variant]}>${amount}</span>;
-  //return <span className={styles[variant]}>₴{Math.round(amount*42)}</span>;
+  return <span className={styles[variant]}>₴{Math.ceil(amount * 42)}</span>;
 };
