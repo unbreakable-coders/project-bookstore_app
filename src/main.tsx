fa842d166b402cbdc9ababcd5602dd3b0802c3d0
Victoriia Kodovbetska <113036948+Pobedasg@users.noreply.github.com>
2025-11-20 16:41:30 +0200

    UC-51: Create Toasts Notifications (#52)

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "./styles/globals.css";
-import App from "./App";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastsContainer } from './components/atoms/Toasts';
+import './styles/globals.css';
+import App from './App';
 
-createRoot(document.getElementById("root") as HTMLElement).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
 );
+
+createRoot(document.getElementById('toast-root')!)?.render(<ToastsContainer />);
