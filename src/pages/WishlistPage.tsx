f004135c946b8094c8a86682b59b6a91a86a0cb6
Julia-Hertseva <gerceva@gmail.com>
2025-11-21 11:47:54 +0200

    fix grid in Favorites section

--- src/pages/WishlistPage.tsx
+++ src/pages/WishlistPage.tsx
@@ -49,7 +49,7 @@ export const WishlistPage: React.FC = () => {
           У вас поки немає улюблених книжок
         </div>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  gap-4 justify-items-center">
           {favouriteBooks.map(book => (
             <BookCard
               key={book.id}
