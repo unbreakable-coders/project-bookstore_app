ab1fe4e3d0c489aeba8973bf520d9d8aaf081815
S-Khraban <s.hraban@i.ua>
2025-11-23 02:46:02 +0200

    Fix: stabilized logo size in header

--- src/components/templates/AppLayout/AppLayout.tsx
+++ src/components/templates/AppLayout/AppLayout.tsx
@@ -2,13 +2,12 @@ import { Outlet } from "react-router-dom";
 import { Header } from "../../organisms/Header";
 import { Footer } from "../../organisms/Footer";
 
-
 export const AppLayout = () => {
   return (
     <div className="min-h-screen flex flex-col bg-background text-foreground">
       <Header />
 
-      <main className="flex-1">
+      <main className="flex-1 pt-16">
         <Outlet />
       </main>
 
