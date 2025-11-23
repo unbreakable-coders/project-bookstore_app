fdc2d1073b4f454cc0109cc68304ba2509fe76d9
S-Khraban <S.Hraban@i.ua>
2025-11-23 05:08:39 +0200

    Connect wishlist + cart to Supabase users, merge LS & DB, badges in header, catalog refactor (#91)

--- src/components/molecules/GlobalLanguageSwitcher/GlobalLanguageSwitcher.tsx
+++ src/components/molecules/GlobalLanguageSwitcher/GlobalLanguageSwitcher.tsx
@@ -3,39 +3,26 @@ import { ICON_BUTTON_CLASS } from '@/components/organisms/Header/Header';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-interface Language {
-  code: string;
-  icon: IconName;
-}
-
 export const GlobalLanguageSwitcher: React.FC = () => {
   const { i18n } = useTranslation();
 
-  const getCurrentLanguage = (): Language => {
-    return i18n.language === 'uk'
-      ? { code: 'en', icon: 'engLang' }
-      : { code: 'uk', icon: 'ukrLang' };
-  };
+  const isUK = i18n.language === 'uk';
 
   const handleLanguageToggle = () => {
-    const currentLang = getCurrentLanguage();
-    void i18n.changeLanguage(currentLang.code);
+    void i18n.changeLanguage(isUK ? 'en' : 'uk');
   };
-
-  const currentLanguage = getCurrentLanguage();
 
   return (
     <button
       onClick={handleLanguageToggle}
       type="button"
       className={`${ICON_BUTTON_CLASS} cursor-pointer`}
-      title={
-        currentLanguage.code === 'uk'
-          ? 'Перейти на українську'
-          : 'Switch to English'
-      }
+      title={isUK ? 'Switch to English' : 'Перейти на українську'}
     >
-      <Icon name={currentLanguage.icon} className="h-5 w-5" />
+      <Icon
+        name={isUK ? 'ukrLang' : 'engLang'} 
+        className="h-5 w-5"
+      />
     </button>
   );
 };
