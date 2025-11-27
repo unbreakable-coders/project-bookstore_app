a63f241b6e2404c7d2e6b4e50ba8c75267bf4fa8
Maksym Shevchuk <47719190+MaksimShe@users.noreply.github.com>
2025-11-27 17:16:32 +0200

    Category to type (#179)
    * changed name of categoties
    
    * changed name of categoties + changed about me (my email)
    
    * fixed

--- src/components/organisms/Home/Categories.tsx
+++ src/components/organisms/Home/Categories.tsx
@@ -45,7 +45,7 @@ export const Categories: FC<Props> = ({ typeBooks }) => {
   return (
     <div className="w-full max-w-[1136px] mx-auto flex flex-col items-center">
       <h2 className="w-full mb-6 text-2xl font-bold text-center">
-        {t('Shop by category')}
+        {t('Shop by type')}
       </h2>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full overflow-visible">
